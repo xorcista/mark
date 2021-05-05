2cc55787688ec2fe5e6a3ff55930e77287912d85
Daniel <xorcista@live.com>
2021-05-05 04:25:11 -0500

    fork

---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd92ba52b1bbb5c66498c67612e7c2ba90caa5f65',
+    'name' => 'mark-php/mark',
+  ),
+  'versions' => 
+  array (
+    'mark-php/mark' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd92ba52b1bbb5c66498c67612e7c2ba90caa5f65',
+    ),
+    'nikic/fast-route' => 
+    array (
+      'pretty_version' => 'v1.3.0',
+      'version' => '1.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '181d480e08d9476e61381e04a71b34dc0432e812',
+    ),
+    'workerman/workerman' => 
+    array (
+      'pretty_version' => 'v4.0.19',
+      'version' => '4.0.19.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'af6025976fba817eeb4d5fbf8d0c1059a5819da3',
+    ),
+  ),
+);
